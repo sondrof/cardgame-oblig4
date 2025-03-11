6ffbda9e5f9152b3d8b9cb1f80c493e65e3d10c0
Sondre Odberg <sondrof@stud.ntnu.no>
2025-03-11 23:42:46 +0100

    Author txt main

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Main.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Main.java
@@ -2,6 +2,9 @@ package no.ntnu.idatx2003.oblig4.cardgame;
 
 /**
  * Main for the Card Game.
+ *
+ * @author Sondre Odberg
+ * @version 0.0.1
  */
 public class Main {
   public static void main(String[] args) {
